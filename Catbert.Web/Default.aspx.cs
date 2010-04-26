52bc60d50e3360626b3249521799e6a76811da6d
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 22:33:37 +0000

    Added new tables "TrackingActions" and "TrackingTypes" to replace the old tracking infrastructure. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 86. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert3;C4118

--- Catbert.Web/Default.aspx.cs
+++ Catbert.Web/Default.aspx.cs
@@ -11,8 +11,11 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        var schools = GenericBLL<School, string>.GetAll();
+        //var schools = GenericBLL<School, string>.GetAll();
 
-        Response.Write(schools.Count);
+        //Response.Write(schools.Count);
+
+        var q = GenericBLL<TrackingType, int>.GetAll();
+
     }
 }
