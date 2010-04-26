6990cee5a6585a2640472ce3fa08de991541ee10
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 22:40:11 +0000

    Added a unitAssociations table which will associate a user to a unit within a specific application.  I also added a class/mapping for this table, and an empty BLL. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 3430. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert3;C4349

---
+++ Catbert.Core/Domain/UnitAssociation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAESDO.Catbert.Core.Domain
+{
+    public class UnitAssociation : DomainObject<UnitAssociation, int>
+    {
+        public virtual bool Inactive { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Application Application { get; set; }
+        public virtual Unit Unit { get; set; }
+
+        public UnitAssociation()
+        {
+
+        }
+    }
+}
