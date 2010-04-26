595e57a6c0496c149c6c4e84a4a0b34db77141bd
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 22:33:27 +0000

    Created a permissions class/mapping, including links to roles/apps/users. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 79. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert3;C4113

---
+++ Catbert.Core/Domain/Permission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAESDO.Catbert.Core.Domain
+{
+    public class Permission : DomainObject<Permission, int>
+    {
+        public virtual bool Inactive { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Application Application { get; set; }
+        public virtual Role Role { get; set; }
+
+        public Permission()
+        {
+
+        }
+    }
+}
