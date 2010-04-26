6cc07f8541b6ad523c825e544cfcf0507b718b28
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 22:33:20 +0000

    Added in the application class/mapping. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 72. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert3;C4108

---
+++ Catbert.Core/Domain/Application.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAESDO.Catbert.Core.Domain
+{
+    public class Application : DomainObject<Application, int>
+    {
+        public virtual string Name { get; set; }
+        public virtual string Abbr { get; set; }
+
+        public virtual string Location { get; set; }
+
+        public virtual string WebServiceHash { get; set; }
+        public virtual string Salt { get; set; }
+
+        public virtual bool Inactive { get; set; }
+
+        public Application()
+        {
+
+        }
+    }
+}
--- Catbert.Web/Default.aspx.cs
+++ Catbert.Web/Default.aspx.cs
@@ -11,6 +11,8 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        //var schools = GenericBLL<School, string>.GetAll();
+        var schools = GenericBLL<School, string>.GetAll();
+
+        Response.Write(schools.Count);
     }
 }
