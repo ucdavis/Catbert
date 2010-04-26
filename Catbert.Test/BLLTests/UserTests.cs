9f68ac4aa18747c8c02d0c70473c2d5ae814e1c3
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 22:41:30 +0000

    Created some testing for catbert so I can test the new user insertion methods which are currently failing. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 3741. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert3;C4417

---
+++ Catbert.Test/BLLTests/UserTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using CAESArch.BLL;
+using CAESArch.Data.NHibernate;
+using CAESDO.Catbert.BLL;
+using CAESDO.Catbert.Core.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHibernate.Cfg;
+
+namespace CAESDO.Catbert.Test.BLLTests
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestInitialize]
+        public void CreateDB()
+        {
+            Configuration config = new Configuration().Configure();
+            //Create the DB using the schema export
+            new NHibernate.Tool.hbm2ddl.SchemaExport(config).Execute(false, true, false, true, NHibernateSessionManager.Instance.GetSession().Connection, null);
+
+            LoadData();
+        }
+
+        private static void LoadData()
+        {
+            LoadUsers();
+        }
+
+        [TestMethod]
+        public void CanGetUser()
+        {
+            var user = UserBLL.GetByID(1);
+
+            Assert.IsNotNull(user);
+            Assert.AreEqual("pjfry", user.LoginID);
+
+        }
+
+        private static void LoadUsers()
+        {
+            var u = new User {FirstName = "Philip", LastName = "Fry", LoginID = "pjfry"};
+
+            using (var ts = new TransactionScope())
+            {
+                UserBLL.EnsurePersistent(u);
+
+                ts.CommitTransaction();
+            }
+        }
+    }
+}
