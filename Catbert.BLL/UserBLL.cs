a68050cd664a6def3d50a4b9c8465b62c786085d
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 22:33:45 +0000

    Added a GetByApplicationRole function, except it doesn't work because of problems searching on the ID properties of any class. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 98. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert3;C4123

--- Catbert.BLL/UserBLL.cs
+++ Catbert.BLL/UserBLL.cs
@@ -8,5 +8,22 @@ namespace CAESDO.Catbert.BLL
 {
     public class UserBLL : GenericBLL<User, int>
     {
+        /// <summary>
+        /// Get a user by login
+        /// </summary>
+        public static User GetUser(string login)
+        {
+            return GetByProperty("LoginID", login);
+        }
+
+        public static List<User> GetByApplicationRole(int applicationID, int roleID)
+        {
+            var query = from user in Queryable
+                        join perm in PermissionBLL.Queryable on user.ID equals perm.ID
+                        where perm.Application.ID == applicationID && perm.Role.ID == roleID
+                        select user;
+
+            return query.ToList();
+        }
     }
 }
