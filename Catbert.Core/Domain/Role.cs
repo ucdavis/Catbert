7baa55c22f1394766bfffafd7530c2e2cf8e38d7
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 22:33:18 +0000

    Created user and role class/mappings (Updated by TFS to TFS Connector, server: http://1x:8080/, id 71. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert3;C4107

---
+++ Catbert.Core/Domain/Role.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAESDO.Catbert.Core.Domain
+{
+    public class Role : DomainObject<Role, int>
+    {
+        public virtual string Name { get; set; }
+
+        public virtual bool Inactive { get; set; }
+
+        public Role()
+        {
+
+        }
+    }
+}
---
+++ Catbert.Core/Domain/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CAESDO.Catbert.Core.Domain
+{
+    public class User : DomainObject<User, int>
+    {
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+
+        public virtual string EmployeeID { get; set; }
+        public virtual string StudentID { get; set; }
+
+        public virtual Guid UserKey { get; set; }
+
+        public virtual bool Inactive { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
