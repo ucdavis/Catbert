79cc3e085c6d528da7188ae515fc3556ce984415
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 22:43:15 +0000

    Added Messages (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4644. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert3;C4515

---
+++ Catbert.Core/Domain/Message.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using CAESArch.Core.Domain;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+
+namespace CAESDO.Catbert.Core.Domain
+{
+    public class Message : DomainObject<Message, int>
+    {
+
+
+    //?      [StringLengthValidator(50)]
+        [NotNullValidator]
+        public virtual string MessageName { get; set; }
+    
+        public virtual string BeginDisplayDate { get; set; }
+
+        public virtual string EndDisplyDate { get; set; }
+        
+        [NotNullValidator] 
+        public virtual bool Isactive { get; set; }
+        public virtual Application Application { get; set; }
+
+    }
+}
