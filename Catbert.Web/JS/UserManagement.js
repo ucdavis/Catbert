0566b0de0a28f5ef970d0bdb8c74dc0662f41ac8
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 22:40:21 +0000

    Moved the entire JS of the UserManagement page to a separate file (UserManagement.js) and then linked back to the userman. page. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 3440. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert3;C4358

---
+++ Catbert.Web/JS/UserManagement.js
@@ -0,0 +1,495 @@
+﻿///<reference path="jquery-1.3.1-vsdoc.js"/>
+
+var baseURL = '../Services/CatbertWebService.asmx/';
+var autocompleteUnitsURL = '../Services/AutocompleteService.asmx/GetUsers';
+
+var application = null;
+
+var search = null, unit = null, role = null; //start with no search, unit, or role filters
+
+var page = 1;
+var pageSize = 3;
+var totalPages = 1;
+
+var sortname = "LastName";
+var sortorder = "ASC";
+
+var userTableDirty = false;
+
+$(document).ready(function() {
+    application = $("#app").val();
+
+    PopulateUserTable(application, search, unit, role, sortname, sortorder); //Populate the user table
+
+    $("#txtSearch").autocomplete(autocompleteUnitsURL, {
+        width: 260,
+        minChars: 2,
+        selectFirst: false,
+        autoFill: false,
+        extraParams: { application: application },
+        formatItem: function(row, i, max) {
+            return row.Name + " (" + row.Login + ")<br/>" + row.Email; //i + "/" + max + ": \"" + row.name + "\" [" + row.to + "]";
+        }
+    }).result(SearchUsers);
+
+    $("#txtSearch").keypress(function(event) {
+        if (event.keyCode == 13) {
+            return SearchUsers();
+        }
+    });
+
+    $("#imgSearch").click(function() {
+        search = "";
+        $("#txtSearch").val(search); //clear out the text
+
+        PopulateUserTable(application, search, unit, role, sortname, sortorder);
+        $(".ac_results").hide(); //Hide the results whenever you hit enter
+
+        return false;
+    });
+
+    $("#txtLoginID").keypress(function(event) {
+        if (event.keyCode == 13) { //On enter, fire the search click event
+            $("#btnSearchUser").click();
+        }
+    });
+    /*
+    $("#tblUsers").tablesorter({
+    headers: { 4: { sorter: false }, 5: { sorter: false} },
+    cssAsc: 'headerSortUp',
+    cssDesc: 'headerSortDown',
+    cssHeader: 'header',
+    widgets: ['zebra']
+    });
+    */
+
+    $(".pager").click(function() {
+        var pagerType = $(this).attr("name");
+
+        if (pagerType == "First") {
+            page = 1;
+        }
+        else if (pagerType == "Last") {
+            page = totalPages;
+        }
+        else if (pagerType == "Previous") {
+            page--;
+            if (page == 0) page = 1;
+        }
+        else { //pagerType = "Next"
+            page++;
+            if (page > totalPages) page = totalPages;
+        }
+
+        PopulateUserTableDefault(application);
+    });
+
+    $("#tblUsers thead tr th.header").click(ChangeSortOrder);
+
+    $(".ShowUserLink").live("click", function() { ShowUserInfo(application, $(this).html()); });
+
+    $("#addUser").click(function() {
+        var findUserDialog = $("#dialogFindUser");
+
+        var buttons = {
+            "Close": function() {
+                $("#divSearchResultsSuccess").hide(0);
+                $(this).dialog("close");
+            }
+        }
+
+        $("#divNewUserNotification").hide(0); //If there was a previous user added, hide the notification
+
+        OpenDialog(findUserDialog, buttons, "Add a User");
+    });
+
+    $("#btnAddUserRole").click(function() { AddUserRole(application); });
+
+    $("#btnAddUserUnit").click(function() { AddUserUnit(application); });
+
+    $("#btnSearchUser").click(function() {
+        $("#spanSearchProgress").show(0); //Show the loading dialog
+        $("#divSearchResultsSuccess").hide(0); //Hide the content
+
+        var data = { eid: null, firstName: null, lastName: null, login: $("#txtLoginID").val() };
+
+        //Call the search service
+        AjaxCall(baseURL + "SearchNewUser", data, SearchNewUserSuccess, null);
+    });
+
+    $("#btnAddUser").click(function() {
+        $("#spanAddUserProgress").show(0); //First show the loading dialog
+
+        //First fill in the user information
+        var user = new Object();
+        user.FirstName = $("#spanNewUserFirstName").html();
+        user.LastName = $("#spanNewUserLastName").html();
+        user.Login = $("#spanNewUserLogin").html();
+        user.Email = $("#txtNewUserEmail").val();
+        user.Phone = $("#txtNewUserPhone").val();
+
+        //Now get the role
+        var role = $("#applicationRoles").val();
+
+        //Now the unit FIS code
+        var unit = $("#units").val();
+
+        AjaxCall(baseURL + "InsertUserWithRoleAndUnit", {
+            serviceUser: user,
+            role: role,
+            unit: unit,
+            application: application
+        },
+                function() { AddUserSuccess(application, search); },
+                null);
+    });
+});
+
+function SearchUsers() {
+    search = $("#txtSearch").val() /*textbox value*/;
+    page = 1; //Change the page when a new search is executed
+    PopulateUserTable(application, search, unit, role, sortname, sortorder);
+
+    setTimeout(function() {
+        $(".ac_results").hide(); //Hide the results whenever you hit enter
+    }, 250);
+
+    return false; //Don't post back
+}
+
+function ChangeSortOrder() {
+    sortname = $(this).attr("title");
+    sortorder = $(this).hasClass("headerSortUp") ? "DESC" : "ASC";
+
+    var sortableHeaders = $(".header");
+    sortableHeaders.removeClass("headerSortUp").removeClass("headerSortDown"); //remove the sort direction classes from each sortable col
+
+    //add the sort direction class back into the current header object
+    if (sortorder == "ASC")
+        $(this).addClass("headerSortUp");
+    else
+        $(this).addClass("headerSortDown");
+
+    //After changing the sortvars, repopulate the grid
+    PopulateUserTableDefault(application);
+}
+
+function AddUserRole(application) {
+    var login = $("#UserInfoLogin").html();
+    var roles = $("#UserInfoRoles");
+    var newrole = $("#UserRoles").val();
+
+    //Find out if the role is already in the roles table
+    var existingRoleMatch = roles.find("tbody tr:visible td:contains(" + newrole + ")").filter(
+                function() {
+                    if ($(this).text() == newrole)
+                        return true;
+                    else
+                        return false;
+                }
+            );
+
+    if (existingRoleMatch.size() == 0) {
+        //Add this role
+        var newrow = CreateRoleRow(newrole, login, application);
+        roles.append(newrow);
+
+        $(newrow).effect("highlight", {}, 3000);
+
+        AjaxCall(baseURL + "AssociateRole",
+                    { login: login, role: newrole, application: application },
+                    null,
+                    null
+                );
+
+        userTableDirty = true; //Users have been modified
+    }
+    else {
+        alert("User already has the role " + newrole);
+    }
+}
+
+function AddUserUnit(application) {
+    var login = $("#UserInfoLogin").html();
+    var units = $("#UserInfoUnits");
+    var newunit = $("#UserUnits").val();
+    var newunitname = $("#UserUnits option:selected").text();
+
+    //Find out if the unitFIS is already in the unit table
+    var existingUnitMatch = units.find("tbody tr:visible td:contains(" + newunit + ")").filter(
+                function() {
+                    if ($(this).text() == newunit)
+                        return true;
+                    else
+                        return false;
+                }
+            );
+
+    if (existingUnitMatch.size() == 0) {
+        //Add the unit
+        var newrow = CreateUnitRow(newunitname, newunit, login, application);
+        units.append(newrow);
+
+        $(newrow).effect("highlight", {}, 3000);
+
+        AjaxCall(baseURL + "AddUnit",
+                    { login: login, application: application, unitFIS: newunit },
+                    null,
+                    null
+                );
+
+        userTableDirty = true; //Users have been modified
+    }
+    else {
+        alert("User already has the unit " + $.trim(newunitname));
+    }
+}
+
+function ShowUserInfo(applicationName, Login) {
+    var dialogUserInfo = $("#dialogUserInfo");
+
+    var buttons = {
+        "Close": function() {
+            $("#divUserInfo").hide(0);
+            $(this).dialog("close");
+        }
+    }
+
+    OpenDialog(dialogUserInfo, buttons, "User Information",
+                function() {
+                    if (userTableDirty) {
+                        PopulateUserTableDefault(applicationName);
+                    }
+                }
+            );
+
+    var baseUrl = baseURL;
+
+    AjaxCall(
+                baseUrl + 'GetUser',
+                { login: Login, application: applicationName },
+                function(data) { PopulateUserInfo(data, applicationName); },
+                null //TODO: Error method
+            );
+}
+
+function PopulateUserInfo(data, application) {
+    $("#UserInfoName").html(data.FirstName + " " + data.LastName);
+    $("#UserInfoLogin").html(data.Login);
+
+    var roles = $("#UserInfoRoles tbody");
+    var units = $("#UserInfoUnits tbody");
+
+    roles.empty();
+    units.empty();
+
+    $(data.Roles).each(function(index, row) {
+        var newrow = CreateRoleRow(row.Name, data.Login, application);
+
+        roles.append(newrow);
+    });
+
+    $(data.Units).each(function(index, row) {
+        var newrow = CreateUnitRow(row.Name, row.UnitFIS, data.Login, application);
+
+        units.append(newrow);
+    });
+
+    $("#divUserInfo").show(0); //Show the user information
+}
+
+function CreateRoleRow(role, login, application) {
+    var newrow = $('<tr></tr>');
+
+    var deleteLink = $('<input type="button" value="X" />');
+    deleteLink.click(function() { DeleteRole(login, role, application, newrow); });
+
+    newrow.append('<td>' + role + '</td>');
+
+    var deleteCol = $('<td>').append(deleteLink);
+    newrow.append(deleteCol);
+
+    return newrow;
+}
+
+function CreateUnitRow(unit, unitFIS, login, application) {
+    var newrow = $('<tr></tr>');
+
+    var deleteLink = $('<input type="button" value="X" />');
+    deleteLink.click(function() { DeleteUnit(login, unitFIS, application, newrow); });
+
+    newrow.append('<td>' + unit + '</td>');
+    newrow.append('<td>' + unitFIS + '</td>');
+
+    var deleteCol = $('<td>').append(deleteLink);
+    newrow.append(deleteCol);
+
+    return newrow;
+}
+
+function DeleteUnit(login, unit, application, rowToDelete) {
+    $(rowToDelete).fadeOut('slow');
+
+    AjaxCall(baseURL + "DeleteUnit",
+                { login: login, application: application, unitFIS: unit },
+                null,
+                null
+            );
+
+    userTableDirty = true; //Users have been modified
+}
+
+function DeleteRole(login, role, application, rowToDelete) {
+    $(rowToDelete).fadeOut('slow');
+
+    AjaxCall(baseURL + "DissociateRole",
+                { login: login, role: role, application: application },
+                null,
+                null
+            );
+
+    userTableDirty = true; //Users have been modified
+}
+
+function AddUserSuccess(application, search) {
+    PopulateUserTable(application, search, null, null, null, null); //Repopulate the table
+
+    $("#dialogFindUser").dialog("close"); //Close the dialog
+    $("#divSearchResultsSuccess").hide(0); //Hide the search results
+    $("#divNewUserNotification").show("slow"); //Show the new user notification
+    $("#spanAddUserProgress").hide(0); //Hide the progress since we are done
+}
+
+function SearchNewUserSuccess(data) {
+
+    var divSearchResults = $("#divSearchResultsSuccess"); //Get the search results div
+    $("#spanSearchProgress").hide(0); //Hide the loading dialog
+
+    if (data.length == 0) {
+        return alert("No Users Found");
+    }
+    else {
+        var user = data[0];
+        $("#spanNewUserFirstName").html(user.FirstName);
+        $("#spanNewUserLastName").html(user.LastName);
+        $("#spanNewUserLogin").html(user.Login);
+        $("#txtNewUserEmail").val(user.Email);
+        $("#txtNewUserPhone").val(user.Phone);
+    }
+
+    divSearchResults.show();
+}
+
+function PopulateUserTableDefault(application) {
+    PopulateUserTable(application, search, null, null, sortname, sortorder);
+}
+
+function PopulateUserTable(application, search, unit, role, sortname, sortorder) {
+    ShowLoadingIndicator(true);
+
+    //Setup the parameters
+    var data = {
+        application: application,
+        search: search,
+        unit: unit,
+        role: role,
+        page: page,
+        pagesize: pageSize,
+        sortname: sortname,
+        sortorder: sortorder
+    };
+
+    //Call the webservice
+    AjaxCall(baseURL + 'GetUsers', data, PopulateUserTableSuccess, null);
+}
+
+function PopulateUserTableSuccess(data) {
+    //data = data.d; //Grab the inner container of data
+
+    //Clear the usertable
+    $("#tblUsersBody").empty();
+
+    //Render out each row
+    $(data.rows).each(RenderRow);
+
+    totalPages = data.total;
+
+    //Populate Page Info
+    $("#spanPageInfo").html(page + " of " + totalPages);
+
+    SortTable();
+    ShowLoadingIndicator(false);
+    userTableDirty = false; //The user table is now up to date
+}
+
+function ShowLoadingIndicator(on) {
+    var loadingDiv = $("#divLoading");
+
+    if (on)
+        loadingDiv.show();
+    else
+        loadingDiv.hide();
+}
+
+function RenderRow(index, row) {
+    var newrow = $('<tr></tr>');
+
+    newrow.append('<td class="ShowUser"><a href="javascript:;" class="ShowUserLink">' + row.Login + '</a></td>');
+    newrow.append('<td class="FirstName">' + row.FirstName + '</td>');
+    newrow.append('<td class="LastName">' + row.LastName + '</td>');
+    newrow.append('<td class="Login">' + row.Login + '</td>');
+    newrow.append('<td class="Email">' + row.Email + '</td>');
+
+    var units = CreateDomFromUserInfoArray(row.Units);
+    var roles = CreateDomFromUserInfoArray(row.Roles);
+
+    newrow.append('<td class="Units">' + units + '</td>');
+    newrow.append('<td class="Roles">' + roles + '</td>');
+
+    $("#tblUsersBody").append(newrow);
+}
+
+//Trigger a sort update, and sort on the last name column
+function SortTable() {
+    /*
+    if ($("#tblUsersBody tr").size() > 0) { //Only resort sort if there are rows to sort
+            
+    $("#tblUsers").trigger("update");
+    var sorting = [[1, 0]];
+    // sort on the first column 
+    $("#tblUsers").trigger("sorton", [sorting]);
+    }
+    */
+}
+
+function CreateDomFromUserInfoArray(array) {
+    var dom = "";
+
+    $(array).each(function(index, obj) {
+        if (index != 0) //For everything other than the first element, include the comma
+            dom += ", ";
+
+        dom += Trim(obj.Name);
+    });
+
+    return dom;
+}
+
+function Trim(str) {
+    return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+}
+
+function OpenDialog(dialog /*The dialog DIV JQuery object*/, buttons /*Button collection */, title, onClose) {
+
+    dialog.dialog("destroy"); //Reset the dialog to its initial state
+    dialog.dialog({
+        autoOpen: true,
+        closeOnEscape: false,
+        width: 600,
+        modal: true,
+        title: title,
+        buttons: buttons,
+        //show: 'fold',
+        close: onClose
+    });
+}
