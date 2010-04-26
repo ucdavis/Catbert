829b26a29439f1a282af230a01953468c511f399
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 22:40:16 +0000

    Updated the get units by user method so that units are only returned if they have a related active unit association (active being the key word). (Updated by TFS to TFS Connector, server: http://1x:8080/, id 3436. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert3;C4354

--- Catbert.BLL/UnitBLL.cs
+++ Catbert.BLL/UnitBLL.cs
@@ -28,7 +28,7 @@ namespace CAESDO.Catbert.BLL
         /// </summary>
         public static List<Unit> GetByUser(string login, string application)
         {
-            var unitAssociations = UnitAssociationBLL.Queryable.Where(assoc => assoc.User.LoginID == login && assoc.Application.Name == application).ToList();
+            var unitAssociations = UnitAssociationBLL.Queryable.Where(assoc => assoc.User.LoginID == login && assoc.Application.Name == application && assoc.Inactive == false).ToList();
 
             return unitAssociations.Select(u => u.Unit).ToList();
         }
