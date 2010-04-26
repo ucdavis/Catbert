54fa001fb17278add16c60872a8853778c68357b
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 22:40:12 +0000

    Oops, restoring the username to the login text. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 3431. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert3;C4350

--- Catbert.Web/Default.aspx.cs
+++ Catbert.Web/Default.aspx.cs
@@ -12,7 +12,7 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        lblLoginID.Text = User.Identity.Name;
     }
 
     protected void btnLogout_Click(object sender, EventArgs e)
