96d954c71012b913539c4f24ee8ab095534026ef
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 22:42:15 +0000

    Added a webservice to the admin section -- it will be used to return information about the selected user, which will be contained within the new service objects class. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4366. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert3;C4451

---
+++ Catbert.Core/ServiceObjects/ServiceObjects.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CAESDO.Catbert.Core.ServiceObjects
+{
+    /// <summary>
+    /// Represents the association of a permission to an application
+    /// </summary>
+    [Serializable]
+    public class PermissionAssociation
+    {
+        public string ApplicationName { get; set; }
+        public string PermissionName { get; set; }
+    }
+
+    /// <summary>
+    /// Represents the association of a unit to an application
+    /// </summary>
+    [Serializable]
+    public class UnitAssociation
+    {
+        public string ApplicationName { get; set; }
+        public string UnitName { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a generalized view of the information for a specific user,
+    /// regardless of the current application
+    /// </summary>
+    [Serializable]
+    public class UserInformation
+    {
+        public string LoginId { get; set; }
+
+        public List<PermissionAssociation> PermissionAssociations { get; set; }
+        public List<UnitAssociation> UnitAssociations { get; set; }
+    }
+}
---
+++ Catbert.Web/App_Code/UserInformationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for UserInformationService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+[System.Web.Script.Services.ScriptService]
+public class UserInformationService : System.Web.Services.WebService
+{
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+}
+
