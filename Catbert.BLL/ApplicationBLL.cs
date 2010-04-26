bd25035c6a5ade30586d6e5507fd4edecbb2ce6f
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 22:33:34 +0000

    Added a tracking class even though there the new tracking has not been developed.  I am moving away from having a tracking table for each type.. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 83. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert3;C4117

--- Catbert.BLL/ApplicationBLL.cs
+++ Catbert.BLL/ApplicationBLL.cs
@@ -3,11 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CAESDO.Catbert.Core.Domain;
+using CAESDO.Catbert.Data;
 
 namespace CAESDO.Catbert.BLL
 {
     public class ApplicationBLL : GenericBLL<Application, int>
     {
+        public static List<Application> GetAll(bool inactive)
+        {
+            var query = from app in Queryable
+                           where app.Inactive == inactive
+                           orderby app.Name
+                            select app;
 
+            return query.ToList<Application>();
+        }
+
+        public static bool SetActiveStatus(int applicationID, bool inactive, string trackingUserName)
+        {
+            //Are there any applications that need to be changed fitting the criteria?
+            bool applicationExists = Queryable.Where(app => app.ID == applicationID && app.Inactive != inactive).Any();
+
+            if (!applicationExists) return false;
+
+            //The application exists, so make the change and track it
+            Application application = Queryable.Where(app => app.ID == applicationID).Single();
+
+            application.Inactive = inactive;
+
+            //Tracking tracking = new Tracking() { UserName = trackingUserName, ActionDate = DateTime.Now };
+
+            /*
+            ApplicationTracking appTracking = new ApplicationTracking() { Application = application, TrackingActionDate = DateTime.Now, TrackingUserName = trackingUserName };
+            appTracking.Comments = string.Format("Application {0} status changed to inactive={1}", applicationID, inactive);
+            appTracking.TrackingType = GetTrackingType(TrackingTypes.Change);
+
+            db.ApplicationTrackings.InsertOnSubmit(appTracking);
+            */
+
+            using (TransactionScope ts = new TransactionScope())
+            {
+                ApplicationBLL.EnsurePersistent(ref application);
+
+                ts.CommittTransaction();
+            }
+
+            return true;
+        }
     }
 }
---
+++ Catbert.Core/Domain/Tracking.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+
+namespace CAESDO.Catbert.Core.Domain
+{
+    public class Tracking : DomainObject<Tracking, int>
+    {
+        [NotNullValidator]
+        public virtual TrackingType Type { get; set; }
+
+        [NotNullValidator]
+        public virtual TrackingAction Action { get; set; }
+
+        [StringLengthValidator(10)]
+        [NotNullValidator]
+        public virtual string UserName { get; set; }
+
+        [NotNullValidator]
+        public virtual DateTime ActionDate { get; set; }
+
+        public virtual string Comments { get; set; }
+
+        public Tracking()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Tracking Types, which include Applications, Permissions, Units, etc.  Anything that can be tracked
+    /// </summary>
+    public class TrackingType : DomainObject<TrackingType, int>
+    {
+        public virtual string Name { get; set; }
+
+        public TrackingType()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Tracking Actions, like Add/Delete/Update
+    /// </summary>
+    public class TrackingAction : DomainObject<TrackingAction, int>
+    {
+        public virtual string Name { get; set; }
+
+        public TrackingAction()
+        {
+
+        }
+    }
+}
