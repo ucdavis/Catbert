f4c770a2bf494d71498316b17617487258759e19
Scott Kirkland <srkirkland@gmail.com>
2010-11-29 22:54:31 +0000

    Added a user service (not yet hooked into IoC) which can do some basic user retrieval and checking.
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert4;C8716

---
+++ Catbert4/Services/UserService.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.Contracts;
+using System.Linq;
+using Catbert4.Core.Domain;
+using UCDArch.Core.PersistanceSupport;
+using System.Security.Principal;
+
+namespace Catbert4.Services
+{
+    public interface IUserService
+    {
+        User GetUser();
+        User GetUser(string login);
+        bool UserExists(string login);
+    }
+
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> _userRepository;
+        private readonly IPrincipal _principal;
+
+        public UserService(IRepository<User> userRepository, IPrincipal principal)
+        {
+            _userRepository = userRepository;
+            _principal = principal;
+        }
+
+        public User GetUser()
+        {
+            Contract.Requires(_principal.Identity.IsAuthenticated);
+
+            return GetUser(_principal.Identity.Name);
+        }
+
+        public User GetUser(string login)
+        {
+            Contract.Requires(login != null);
+
+            return _userRepository.Queryable.Where(x => x.LoginId == login).Single();
+        }
+
+        public bool UserExists(string login)
+        {
+            Contract.Requires(login != null);
+
+            return _userRepository.Queryable.Any(x => x.LoginId == login);
+        }
+    }
+}
