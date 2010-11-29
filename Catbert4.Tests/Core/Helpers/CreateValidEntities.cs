ab1b9a70c303aafd8f47966dba110f88efbb7344
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2010-11-29 23:41:33 +0000

    Added Framework for Unit Tests
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Catbert/Trunk/Catbert4;C8722

---
+++ Catbert4.Tests/Core/Helpers/CreateValidEntities.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Catbert4.Tests.Core.Helpers
+{
+    public static class CreateValidEntities
+    {
+        #region Helper Extension
+
+        private static string Extra(this int? counter)
+        {
+            var extraString = "";
+            if (counter != null)
+            {
+                extraString = counter.ToString();
+            }
+            return extraString;
+        }
+
+        #endregion Helper Extension
+
+        //Sample:
+        /*
+        public static User User(int? counter)
+        {
+            var rtValue = new User();
+            rtValue.Email = "Email" + counter.Extra();
+            rtValue.LoginId = "LoginId" + counter.Extra();
+            rtValue.FirstName = "FirstName" + counter.Extra();
+            rtValue.LastName = "LastName" + counter.Extra();
+            return rtValue;
+        }
+         */
+    }
+}
