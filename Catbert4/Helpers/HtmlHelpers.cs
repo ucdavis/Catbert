fbb0f18af380406e88f2b7c40228cf0a01f0278a
Scott Kirkland <srkirkland@gmail.com>
2012-09-19 11:32:09 -0700

    rewrote tag builder to not use web.webpages

--- Catbert4/Helpers/HtmlHelpers.cs
+++ Catbert4/Helpers/HtmlHelpers.cs
@@ -12,12 +12,13 @@ namespace Catbert4.Helpers
         public static HtmlString IncludeJqueryTemplate()
         {
             var jqueryTemplateUrl = WebConfigurationManager.AppSettings["jqueryTemplateUrl"];
-            
-            var t = new TagBuilder("script");
-            t.Attributes.Add("type", "text/javascript");
-            t.Attributes.Add("src", jqueryTemplateUrl);
 
-            return new HtmlString(t.ToString());
+            var template = string.Format(@"<script type='text/javascript' src='{0}'></script>", jqueryTemplateUrl);
+            //var t = new TagBuilder("script");
+            //t.Attributes.Add("type", "text/javascript");
+            //t.Attributes.Add("src", jqueryTemplateUrl);
+
+            return new HtmlString(template);
         }
     }
 }
